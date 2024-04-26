5ddc12d778476dcc103aee455b1e25a3ac77f5ad
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-04-26 01:40:19 -0300

    Module 01-Aula 06

--- aula01/a1.3/pages/sobre.js
+++ aula01/a1.3/pages/sobre.js
@@ -1,9 +1,15 @@
 function SobrePage() {
-    return (
-      <div>
-        Você está na página sobre
-      </div>
-    )
-  }
-  
-  export default SobrePage;
+  return (
+    <div>
+      Você está na página sobre
+
+      <ul>
+        <li>
+          <a href="/">Ir para a Home</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default SobrePage;
